7c27ab10fbfb4cf156576063f3d831945799eebb
Saurabh Jha <saurabh.jhaa@gmail.com>
2014-01-30 14:28:41 +0530

    Renaming get_extension to get_file_extension

--- converters/document_converter.py
+++ converters/document_converter.py
@@ -3,11 +3,11 @@ sys.path.append('converters/')
 
 from general import GeneralConverter
 from utilities import class_selector
-from utils import get_extension
+from utils import get_file_extension
 
 def convert(input_files_objects, output_formats):
     sample_input_file = input_files_objects[0].input_file_path
-    input_format = get_extension(sample_input_file)
+    input_format = get_file_extension(sample_input_file)
     converters = [class_selector(input_format, output_format)
                   for output_format in output_formats]
     result = []
