767f36bbb945a910033d11cbd4806ec137d425ff
Sushant Gupta <sushant711@gmail.com>
2014-01-31 15:26:19 +0530

    Indentation to spaces from tabs

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 class ENVIRONMENT:
-	development = 'development'
-	production = 'production'
+    development = 'development'
+    production = 'production'
 
 DEBUG = True
 ENV = os.environ.get('APP_ENVIRONMENT', ENVIRONMENT.development)
@@ -16,7 +16,7 @@ S3_BUCKET = 'document_converter'
 S3_DUMP_FOLDER = 'test-output'
 
 if ENV == ENVIRONMENT.production:
-	S3_DUMP_FOLDER = 'output'
+    S3_DUMP_FOLDER = 'output'
 
 POSTGRES_DB_URI = 'postgresql://rbox:rbox@localhost:5432/document_converter'
 SECRET_KEY = '5ryNFKc13vaz8ABzMujbxFqvTerIqwNXrunGF14P'
