03b8346495e992eb9f68bb72cb14c60946b847a4
Saurabh Jha <saurabh.jhaa@gmail.com>
2014-02-03 09:59:07 +0530

    Clarifying variable names. Changing interm to interim in converters/document_converter.py

--- converters/document_converter.py
+++ converters/document_converter.py
@@ -12,14 +12,14 @@ def convert(input_files_objects, output_formats):
     input_format = get_file_extension(sample_input_file)
     converters_list = [class_selector(input_format, output_format)
                   for output_format in output_formats]
-    interm_files_objects = input_files_objects
+    interim_files_objects = input_files_objects
     for converter, expression in converters_list:
-        obj = converter(interm_files_objects)
-        interm_files_objects = obj.convert()
-        output_files_objects = [FileManager(interm_file_object.output_file_path)
-                               for interm_file_object in interm_files_objects]
-        interm_files_objects = output_files_objects
-    for input_file_object, interm_file_object in input_files_objects interm_files_objects:
+        obj = converter(interim_files_objects)
+        interim_files_objects = obj.convert()
+        output_files_objects = [FileManager(interim_file_object.output_file_path)
+                               for interim_file_object in interim_files_objects]
+        interim_files_objects = output_files_objects
+    for input_file_object, interim_file_object in input_files_objects interim_files_objects:
         input_file_object.converted = True
-        input_file_object.output_file_path = interm_files_objects.get_output_file_path()
+        input_file_object.output_file_path = interim_files_objects.get_output_file_path()
     return input_files_objects
