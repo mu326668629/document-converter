2ba0f819b6795f512199c7ff38e05da83df706f4
Sushant Gupta <sushant711@gmail.com>
2014-02-07 11:32:20 +0530

    Removed db connection from config

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 
 class ENVIRONMENT:
     development = 'development'
@@ -13,7 +12,8 @@ UPLOAD_FOLDER = 'tmp'
 OUTPUT_FOLDER = 'output/'
 
 S3_BUCKET = 'document_converter'
-S3_DUMP_FOLDER = 'output'
+REMOTE_INPUT_FOLDER = 'input'
+REMOTE_DUMP_FOLDER = 'output'
 
 POSTGRES_DB_URI = 'postgresql://rbox:rbox@localhost:5432/document_converter'
 SECRET_KEY = '5ryNFKc13vaz8ABzMujbxFqvTerIqwNXrunGF14P'
@@ -26,5 +26,3 @@ app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 app.config.from_object(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = app.config['POSTGRES_DB_URI']
-
-db = SQLAlchemy(app)
