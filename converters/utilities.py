c65120a358aa2f0722937fd4ed418a45ae9d5b2b
Saurabh Jha <saurabh.jhaa@gmail.com>
2014-02-14 11:51:31 +0530

    Simplifying class_selector. Now it is easy to debug

--- converters/utilities.py
+++ converters/utilities.py
@@ -18,24 +18,21 @@ AVAILABLE_CONVERTERS = [(HtmlPdf, 'htmlpdf'), (HtmlTxt, 'htmltxt'), (PdfHtml, 'p
 FORMAT_RE = [re.compile('.*%s'%fmt) for fmt in AVAILABLE_FORMATS]
 
 def class_selector(input_format, output_format):
-    input_format_re = re.compile('%s.*'%input_format)
-    output_format_re =  re.compile('.*%s'%output_format)
-    result = [(converter, expression) for converter, expression in AVAILABLE_CONVERTERS
-              if input_format_re.match(expression) and output_format_re.match(expression)]
-    if not result:
-        result1 = [(converter, expression) for converter, expression in AVAILABLE_CONVERTERS
-                   if input_format_re.match(expression)]
-        result2 = [(converter, expression) for converter, expression in AVAILABLE_CONVERTERS
-                   if output_format_re.match(expression)]
-        for converter, expression in result1:
-            result.append((converter, expression))
-            input_format_c = expression[len(input_format):]
-            input_format_c_re = re.compile('%s.*'%input_format_c)
-            if not class_selector(input_format_c, output_format):
-                result = result[:-1]
-            else:
-                result.append(class_selector(input_format_c, output_format)[0])
-    return result
+    input_format_re = re.compile('^%s.*$'%input_format)
+    output_format_re = re.compile('^.*%s$'%output_format)
+    converter1 = [(converter, expression) for converter, expression in AVAILABLE_CONVERTERS
+                  if input_format_re.match(expression)]
+    converter2 = [(converter, expression) for converter, expression in AVAILABLE_CONVERTERS
+                  if output_format_re.match(expression)]
+    converters = list(set(converter1) & set(converter2))
+    if not converters:
+        input_re = re.compile('^%s'%input_format)
+        interim_formats = [input_re.sub('', expression) for converter, expression in converter1]
+        for i, interim_format in enumerate(interim_formats):
+            converters = class_selector(interim_format, output_format)
+            if converters:
+                converters = [converter1[i], converters]
+    return converters
 
 def remove_duplicates(converters_list):
     product = []
@@ -44,3 +41,17 @@ def remove_duplicates(converters_list):
             if converter not in product:
                 product.append(converter)    
     return product
+
+def check_if_cjk(str):
+    '''
+    rudimentary CJK detection
+    seems like a lot of it is between these two ranges.
+    cf. http://www.alanwood.net/unicode/fontsbyrange.html
+    '''
+    ranges = [(12272, 12287), (19968, 40959)]
+    for i in str:
+        j = ord(i)
+        for (low, high) in ranges:
+            if j > low and j < high:
+                return True
+    return False
