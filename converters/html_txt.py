2272a5bd62165c88cf9725738c1ade99a90f362e
Saurabh Jha <saurabh.jhaa@gmail.com>
2014-02-19 09:49:34 +0530

    Fixing a bug in html_txt

--- converters/html_txt.py
+++ converters/html_txt.py
@@ -24,7 +24,10 @@ class HtmlTxt(GeneralConverter):
         if input_file_object:
             h = html2text.HTML2Text()
             h.ignore_links = h.ignore_images = True
-            input_stream = input_file_object.get_input_stream()
+            try:
+                input_stream = input_file_object.get_input_stream()
+            except IOError:
+                return None
             soup = BeautifulSoup(input_stream)
             invalidAttrs = 'href src width height target style color face size script'.split()
             for attr in invalidAttrs:
