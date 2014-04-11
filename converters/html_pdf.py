df5f4f6265c9e5e2d0af09105bf11f5de0b6d0c9
Vedarth Kulkarni <vedarthk@vedarthz.in>
2014-04-11 18:01:48 +0530

    Fix in HTML to PDF converter.

--- converters/html_pdf.py
+++ converters/html_pdf.py
@@ -3,13 +3,15 @@ import os
 
 sys.path.insert(0, '..')
 
-
-CONVERTER_LOCATION = 'libreoffice --headless --convert-to pdf'
+from config import UPLOAD_FOLDER
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+TMP_DIR = os.path.join(PARENT_DIR, UPLOAD_FOLDER)
+CONVERTER_LOCATION = 'libreoffice --headless --convert-to pdf --outdir {}'.format(TMP_DIR)
 
 
 from general import GeneralConverter
 from utils import rename_filename_with_extension
-from config import UPLOAD_FOLDER
+
 
 
 class HtmlPdf(GeneralConverter):
@@ -23,16 +25,13 @@ class HtmlPdf(GeneralConverter):
 
     def _single_convert(self, input_file_object):
         if input_file_object:
-            input_file_path = input_file_object.get_input_file_path()
+            input_file_path = os.path.join(
+                PARENT_DIR,
+                input_file_object.get_input_file_path())
             output_file_name = rename_filename_with_extension(
                 os.path.basename(input_file_path), 'pdf')
             os.system('%s %s'%(CONVERTER_LOCATION, input_file_path))
-            try:
-                open(output_file_name)
-                os.system('mv %s %s'%(output_file_name, UPLOAD_FOLDER))
-                return os.path.join(UPLOAD_FOLDER, output_file_name)
-            except IOError:
-                print "Conversion Unsuccessfull for html_pdf"
-                return None
+            output_file_name = os.path.join(tmp_dir, output_file_name)
+            return output_file_name
         else:
             return None
