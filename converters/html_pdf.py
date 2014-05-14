b874cd303e2f637f99a004576050af0d31a44eb8
Kracekumar Ramaraju <me@kracekumar.com>
2014-05-15 01:50:47 +0530

    Remove iframe and disable js

--- converters/html_pdf.py
+++ converters/html_pdf.py
@@ -1,3 +1,4 @@
+import time
 import sys
 import os
 import subprocess
@@ -9,8 +10,10 @@ from logger import log
 from config import UPLOAD_FOLDER
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 TMP_DIR = os.path.join(PARENT_DIR, UPLOAD_FOLDER)
+REMOVE_IFRAME = """sed 's/<iframe[^>]*>.*<\/iframe>//g'\
+ {input_file_path} > {intermediate_path}"""
 CONVERTER_LOCATION = '''xvfb-run\
- /usr/bin/wkhtmltopdf {input_file_path} {output_file_path}'''
+ /usr/bin/wkhtmltopdf --disable-javascript {input_file_path} {output_file_path}'''
 
 
 from general import GeneralConverter
@@ -31,12 +34,20 @@ class HtmlPdf(GeneralConverter):
             input_file_path = input_file_object.get_input_file_path()
             output_file_name = rename_filename_with_extension(
                 os.path.basename(input_file_path), 'pdf')
-
+            
+            intermediate_filename = str(time.time()).replace('.', '') + '.html'
             output_file_path = os.path.join(TMP_DIR, output_file_name)
+            intermediate_path = os.path.join(TMP_DIR, intermediate_filename)
+
+            cleaner = REMOVE_IFRAME.format(
+                input_file_path=input_file_path,
+                intermediate_path=intermediate_path)
+            subprocess.call(cleaner.split())
+
             converter = CONVERTER_LOCATION.format(
                 input_file_path=input_file_path,
                 output_file_path=output_file_path)
-
+            
             subprocess.call(converter.split())
             if os.path.isfile(output_file_path):
                 return output_file_path
